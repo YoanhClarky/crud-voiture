231a0bfd3b72ead9c01d37a16c0f5122be7c6f9c
yoanhmantele <yoanh.mantele@ginov.io>
2024-12-17 13:15:46 +0100

    add

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,28 @@
 import type { NextConfig } from "next";
 
 const config: NextConfig = {
-    experimental: {
-        serverActions: {
-            bodySizeLimit: '2mb',
-            allowedOrigins: ['*']
-        }
-    },
-    swcMinify: false, // Désactive la minification de SWC
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+      allowedOrigins: ['*']
+    }
+  },
+  swcMinify: false, // Désactive la minification de SWC
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // Ne pas inclure ces modules côté client
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
+  // Assurez-vous que NextAuth.js peut être résolu correctement
+  transpilePackages: ['next-auth'],
 };
 
 export default config;
+
